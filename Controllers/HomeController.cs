435d96165fc2da5f3381bff8dcc4a762a9530305
krishna-bahadur <krishnabk651@gmail.com>
2024-01-04 12:51:59 +0545

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+using GoogleAIStudio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GoogleAIStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ImageClassificationModel model)
+        {
+            try
+            {
+                var imageData = "";
+                if (model.Image != null)
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        model.Image!.CopyTo(stream);
+                        byte[] imageBytes = stream.ToArray();
+                        imageData = Convert.ToBase64String(imageBytes);
+                    }
+                }
+
+                string apiKey = "API-KEY";
+                string apiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=" + apiKey;
+
+                using (HttpClient httpClient = new HttpClient())
+                {
+
+                    string jsonPayload = $@"
+            {{
+                ""contents"": [
+                    {{
+                        ""parts"": [
+                            {{
+                                ""text"": ""{model.Description}""
+                            }},
+                            {{
+                                ""text"": ""Image: ""
+                            }},
+                            {{
+                                ""inlineData"": {{
+                                    ""mimeType"": ""image/jpeg"",
+                                    ""data"": ""{imageData}""
+                                }}
+                            }}
+                        ]
+                    }}
+                ],
+                ""generationConfig"": {{
+                    ""temperature"": 0.9,
+                    ""topK"": 40,
+                    ""topP"": 0.95,
+                    ""maxOutputTokens"": 1024,
+                    ""stopSequences"": []
+                }},
+                ""safetySettings"": [
+                    {{
+                        ""category"": ""HARM_CATEGORY_HARASSMENT"",
+                        ""threshold"": ""BLOCK_MEDIUM_AND_ABOVE""
+                    }},
+                    {{
+                        ""category"": ""HARM_CATEGORY_HATE_SPEECH"",
+                        ""threshold"": ""BLOCK_MEDIUM_AND_ABOVE""
+                    }},
+                    {{
+                        ""category"": ""HARM_CATEGORY_SEXUALLY_EXPLICIT"",
+                        ""threshold"": ""BLOCK_MEDIUM_AND_ABOVE""
+                    }},
+                    {{
+                        ""category"": ""HARM_CATEGORY_DANGEROUS_CONTENT"",
+                        ""threshold"": ""BLOCK_MEDIUM_AND_ABOVE""
+                    }}
+                ]
+            }}";
+
+                    // Create the HTTP request
+                    StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+                    // Handle the response
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        dynamic responseObject = JObject.Parse(responseBody);
+
+                        string text = responseObject?.candidates[0]?.content?.parts[0]?.text ?? "";
+                        if (!string.IsNullOrEmpty(text))
+                        {
+                            return Json(new { success = true, message = text });
+                        }
+                        else
+                        {
+                            string message = "Sorry, We Could not find the response.";
+                            return Json(new { success = true, message = message });
+
+
+                        }
+                    }
+                    else
+                    {
+                        string message = "API Request failed.";
+                        return Json(new { success = true, message = message });
+                    }
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message);
+            }
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,84 @@
+﻿function scrollToSection() {
+    var targetElement = document.querySelector('.AI_section');
+
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+function displayImage(input) {
+    var img = document.getElementById('uploadedImage');
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            img.src = e.target.result;
+            img.width = 200; 
+            img.height = 200; 
+            img.style.objectFit = 'cover';
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function AppendContent(button) {
+    var buttonText = button.textContent || button.innerText;
+    $('#Description').val(buttonText);
+}
+
+
+$(document).ready(function () {
+    // Handle form submission
+    $('#aiForm').submit(function (event) {
+
+        $('.ai-reposne').hide();
+        $('#lds-roller').show();
+
+
+        event.preventDefault();
+
+        var formData = new FormData($(this)[0]);
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action'),
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (result) {
+                if (result.success) {
+                    simulateTyping(result.message, $('.response'));
+                }
+            },
+            error: function (error) {
+                console.error('Error submitting form', error);
+            },
+            complete: function () {
+                $('#lds-roller').hide();
+                $('.ai-reposne').show();
+            }
+        });
+    });
+});
+
+function simulateTyping(text, targetElement) {
+    var index = 0;
+
+    function addNextCharacter() {
+        var char = text[index];
+        if (char === '\n') {
+            targetElement.append('<br>');
+        } else {
+            targetElement.append(char);
+        }
+        index++;
+
+        if (index < text.length) {
+            setTimeout(addNextCharacter, 20); 
+        }
+    }
+
+    addNextCharacter();
+}
